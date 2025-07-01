b864ee910ee8fdf72773c873476a4cf518216535
apexcoder007 <saurabhdubey96440@gmail.com>
2025-07-01 19:25:59 +0530

    created RedisServer header

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "../include/RedisServer.h"
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    int port = 6379; //default port 
+    if(argc>=2)
+        port = std::stoi(argv[1]);
+    RedisServer server(port);
+}
