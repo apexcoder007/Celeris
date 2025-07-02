b86c2ea0bb15e8dc4091669ddcb87a988c64608b
apexcoder007 <saurabhdubey96440@gmail.com>
2025-07-02 15:21:29 +0530

    added dumping to database functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include "../include/RedisServer.h"
+#include "../include/RedisDatabase.h"
 #include <iostream>
-#include <thread> 
-#include <chrono> 
+#include <thread>
+#include <chrono>
 
-int main(int argc, char* argv[]){
-    int port = 6379; //default port 
-    if(argc>=2)
+int main(int argc, char *argv[])
+{
+    int port = 6379; // default
+    if (argc >= 2)
         port = std::stoi(argv[1]);
+
+    if (RedisDatabase::getInstance().load("dump.my_rdb"))
+        std::cout << "Database Loaded From dump.my_rdb\n";
+    else
+        std::cout << "No dump found or load failed; starting with an empty database.\n";
+
     RedisServer server(port);
-    std::thread persistenceThread([](){
-        while(true){
+
+    // Backgroung persistance: dump the database every 300 seconds. (5 * 60 seconds save database)
+    std::thread persistanceThread([]()
+                                  {
+        while (true) {
             std::this_thread::sleep_for(std::chrono::seconds(300));
+            if (!RedisDatabase::getInstance().dump("dump.my_rdb"))
+                std::cerr << "Error Dumping Database\n";
+            else 
+                std::cout << "Database Dumped to dump.my_rdb\n";
         } });
-    persistenceThread.detach();
+    persistanceThread.detach();
 
     server.run();
     return 0;
